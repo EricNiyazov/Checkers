83edc80e65d8639f88b6bacb206f274ae3afd6d1
Eric Niyazov <ERIC@Margaritas-iMac.home>
2019-03-15 19:32:51 -0400

    A custom function creates the square in the window

--- ENcheckers.py
+++ ENcheckers.py
@@ -1,8 +1,12 @@
 from graphics import *
 
+def draw_sqr (cX, cY, size, win):
+    square = Rectangle(Point(cX, cY), Point(cX + size, cY + size))
+    square.setFill(color_rgb(230,0 ,0 ))
+    square.draw(win)
+
 
 chwin = GraphWin("Checkers.py", 500, 500)
 
-square = Rectangle(Point(0, 0), Point(50, 50))
-square.setFill(color_rgb(230,0 ,0 ))
-square.draw(chwin)
+
+draw_sqr (0,0,50,chwin)
